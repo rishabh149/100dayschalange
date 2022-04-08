6c8427eaefa937afe827593659af58d1a2891d41
rishabh149 <81417746+rishabh149@users.noreply.github.com>
2022-04-08 22:35:31 +0530

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		try {
+		    Scanner sc=new Scanner(System.in);
+		    int t=sc.nextInt();
+		    while(t>0)
+		    {
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if(y>x)
+		       System.out.println("NO");
+		    else
+		        System.out.println("YES");
+		
+		    t--;
+		}  
+		    
+		    
+		} catch(Exception e) {
+		return ;
+		}
+	}
+}
